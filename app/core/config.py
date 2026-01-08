90d8d6ddbdf2b0f3d25f8331485f76889ae6870c
lruiz-dcs <lruiz@dirconsolutions.com>
2026-01-07 19:41:22 -0500

    Delete app/core/config.py
    no deberia haber subido

--- app/core/config.py
+++
@@ -1,8 +0,0 @@
-
-
-class Config():
-    SERVER_HOST              = "192.168.1.10"
-    SERVER_DATABASE          = "bdAuxsql"
-    SERVER_DATABASE_USERNAME = "sa"
-    SERVER_DATABASE_PASSWORD = "vwsqlserver"
-    
